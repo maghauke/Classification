519d155790634128d8333bb192ecbaac9f8a2b4a
Magnus Haukeland <magnushaukeland@Magnuss-MacBook-Pro.local>
2020-04-30 15:11:21 +0200

    Finished iris

--- Classification-project/Iris/MSE.m
+++
@@ -1,9 +0,0 @@
-function mse = MSE(X, T, W, g, N)
-    mse = 0;
-    for k = 1:N
-        gk = g(X(:,k),W);
-        tk = T(:,k);        
-        mse = mse + 0.5 * (gk-tk)'*(gk-tk);
-    end
-end
-
--- Classification-project/Iris/MSE_grad.m
+++
@@ -1,9 +0,0 @@
-function gradient = MSE_grad(X,T,W, g)
-    gradient = zeros(size(W));
-    for k = 1:size(X,2)
-        xk = X(:,k);
-        tk = T(:,k);
-        gk = g(xk,W);
-        gradient = gradient + ((gk-tk) .* gk .* (1-gk))*xk.';    % elementwise multiplication    
-    end
-end
